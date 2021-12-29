e4205d497b9c0fa0f9a0817485c37a69fc217b84
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-12-29 07:30:34 -0600

    return only branch

--- src/graphql/resolvers/Mutation/createBranch.js
+++ src/graphql/resolvers/Mutation/createBranch.js
@@ -14,6 +14,6 @@ export default async function createBranch(_, { input }, context) {
   const branch = await context.mutations.createBranch(context, {
     ...mutationInput
   });
-
-  return { branch };
+  console.log(["branch", branch);
+  return branch;
 }
