9fcf5e5673a8df0ebedef38b9ee485a27d06076c
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-12-30 14:12:20 -0600

    add query is available branch

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -2,5 +2,12 @@ import branchById from "./branchById.js";
 import branches from "./branches.js";
 import branchWithoutPagination from "./branchWithoutPagination.js";
 import metaddress from "./metaddress.js";
+import isAvailableBranch from "./isAvailableBranch.js";
 
-export default { branchById, branches, branchWithoutPagination, metaddress };
+export default {
+  branchById,
+  branches,
+  branchWithoutPagination,
+  metaddress,
+  isAvailableBranch
+};
